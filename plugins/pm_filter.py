b885e1259a1fa316b04c693e5a5b21cfbeead9e1
TOM HOLLAND <93207390+TOMHOLLAND2007@users.noreply.github.com>
2021-11-08 14:05:52 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -71,7 +71,7 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("സോറിട്ടോ പക്ഷെ ഇവിടെ നിനക്ക് ഉള്ളത് ഒന്നുമില്ല😤", show_alert=True)
     try:
         offset = int(offset)
     except:
